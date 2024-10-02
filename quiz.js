f54e8b608377fc57908f446f004f21facdf56663
mukuru1 <kabengaedwin@gmail.com>
2024-10-02 12:22:51 +0200

    edited quiz.js

--- quiz.js
+++ quiz.js
@@ -21,6 +21,17 @@ function checkAnswer() {
         // If no answer is selected, prompt the user to choose one
         feedbackElement.textContent = "Please select an answer before submitting.";
     }
+    if (userAnswer) {
+        // Compare user's answer to the correct answer
+        if (userAnswer.value === correctAnswer) {
+            feedbackElement.textContent = "Correct! Well done.";
+        } else {
+            feedbackElement.textContent = "That's incorrect. Try again!";
+        }
+    } else {
+        // If no answer is selected, prompt the user to choose one
+        feedbackElement.textContent = "Please select an answer before submitting.";
+    }
 }
 
 // Add event listener to the submit button
